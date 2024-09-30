876c9e019cfc14e2ff65cb8e9f7896e037e446a5
Axel Cueva <axel.cueva@unmsm.edu.pe>
2024-09-30 13:09:52 -0500

    ga

--- src/modelo/ConciertoArreglo.java
+++ src/modelo/ConciertoArreglo.java
@@ -9,6 +9,58 @@ package modelo;
  *
  * @author OCA-UIE
  */
+package modelo;
+
 public class ConciertoArreglo {
-    
+
+    private Concierto[] conciertos;
+    private int indice;
+
+    public ConciertoArreglo(int cantidad) {
+        indice = 0;
+        conciertos = new Concierto[cantidad];
+    }
+
+    public boolean agregar(Concierto obj) {
+        if (!estaLleno()) {
+            conciertos[this.indice] = obj;
+            this.indice++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaLleno() {
+        return this.indice >= conciertos.length;
+    }
+
+    public boolean eliminarConcierto(Concierto obj) {
+        for (int i = 0; i < this.indice; i++) {
+            if (conciertos[i].equals(obj)) {
+                for (int j = i; j < this.indice - 1; j++) {
+                    conciertos[j] = conciertos[j + 1];
+                }
+                conciertos[this.indice - 1] = null;
+                this.indice--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Concierto buscarPorNombre(String nombre) {
+        for (int i = 0; i < this.indice; i++) {
+            if (conciertos[i].getNombre().equalsIgnoreCase(nombre)) {
+                return conciertos[i];
+        }
+        return null;
+    }
+
+    public int cantidadDeConciertos() {
+        return this.indice;
+    }
+
+    public int capacidadMaxima() {
+        return conciertos.length;
 }
+
